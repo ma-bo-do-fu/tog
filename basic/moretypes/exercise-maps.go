4583d1d8667c42d551fea009b030f3def68f0d95
ma-bo-do-fu <t11a.256.512.1024@gmail.com>
2018-07-31 17:25:48 +0900

    finished exercise-maps.go

---
+++ basic/moretypes/exercise-maps.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	wordcount := make(map[string]int)
+	for _, words := range strings.Fields(s) {
+		wordcount[words]++
+	}
+	return wordcount
+}
+
+func main() {
+	wc.Test(WordCount)
+}
+
+/*
+PASS
+ f("I am learning Go!") =
+  map[string]int{"learning":1, "Go!":1, "I":1, "am":1}
+PASS
+ f("The quick brown fox jumped over the lazy dog.") =
+  map[string]int{"The":1, "brown":1, "fox":1, "over":1, "quick":1, "jumped":1, "the":1, "lazy":1, "dog.":1}
+PASS
+ f("I ate a donut. Then I ate another donut.") =
+  map[string]int{"I":2, "ate":2, "a":1, "donut.":2, "Then":1, "another":1}
+PASS
+ f("A man a plan a canal panama.") =
+  map[string]int{"a":2, "plan":1, "canal":1, "panama.":1, "A":1, "man":1}
+ */
